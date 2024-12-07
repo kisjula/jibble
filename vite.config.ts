29a713cfe3cbc4eb4c299f6eadebc786dfee5d3d
kisjula <kisjula@gmail.com>
2024-12-07 13:05:56 +0100

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import vueDevTools from 'vite-plugin-vue-devtools'
 import pugPlugin from 'vite-plugin-pug'
 
 // https://vite.dev/config/
+console.log(process)
 export default defineConfig({
-    plugins: [vue(), vueDevTools(), pugPlugin()],
+    plugins: [vue() /* vueDevTools() */, , pugPlugin()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url)),
