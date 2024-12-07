6ae48b50b38c47bf1e3a4a4b403218141de4743e
kisjula <kisjula@gmail.com>
2024-12-07 13:07:05 +0100

    Cypress test load

---
+++ cypress/e2e/load.cy.ts
@@ -0,0 +1,45 @@
+import { useMovieStore } from '../../src/stores/movie'
+import { createPinia, setActivePinia } from 'pinia'
+import type { Meta } from '../../src/interfaces/movie.interface'
+
+describe('Pagination test', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+
+    const API = 'https://jsonmock.hackerrank.com/api/movies/search/'
+    const toList = '[data-testid=nav-list]'
+    const toFavourites = '[data-testid=nav-favourites]'
+
+    const movieListItem = '[data-testid=movie-list-item]'
+    const favouriteButton = '[data-testid=set-favourite]'
+    const starSolid = '[data-testid=star-solid]'
+    const starRegular = '[data-testid=star-regular]'
+
+    it('passes', () => {
+        cy.visit('/')
+
+        cy.intercept('GET', `${API}?page=1`).as('getMovies')
+
+        cy.wait(['@getMovies'])
+        cy.get(toFavourites).click()
+
+        cy.contains('h1', 'Favourite movies')
+
+        cy.get(toList).click()
+        cy.wait(['@getMovies'])
+
+        cy.contains('h1', 'Browse movies')
+
+        cy.get(movieListItem).eq(0).find(starSolid).should('not.exist')
+        cy.get(movieListItem).eq(0).find(starRegular).should('exist')
+        cy.get(movieListItem).eq(0).find(favouriteButton).click()
+        cy.get(movieListItem).eq(0).find(starSolid).should('exist')
+        cy.get(movieListItem).eq(0).find(starRegular).should('not.exist')
+
+        cy.reload()
+
+        cy.get(movieListItem).eq(0).find(starSolid).should('exist')
+        cy.get(movieListItem).eq(0).find(starRegular).should('not.exist')
+    })
+})
